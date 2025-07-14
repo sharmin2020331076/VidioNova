846cf537284c8c876e7f343d0a4a215d55d80586
sharmin2020331076 <sharminjuthi1080@gmail.com>
2025-07-15 01:29:19 +0600

    added App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,43 @@
 import { Navbar } from "./components/Navbar";
+import { Feature } from "./pages/Feature";
+import { Hero } from "./pages/Hero";
+import { SocialTools } from "./pages/SocialTools";
+import { Tail } from "./pages/Tail";
+import { Testimonial } from "./pages/Testimonial";
+import { Works } from "./pages/Works";
 
-function App(){
-  return(
-   <main className="min-w-screen min-h-screen">
-    <Navbar/>
-   </main>
-  )
+function App() {
+  return (
+   
+     <div className="relative min-h-screen w-full bg-gradient-to-br from-[#35015e] via-black to-black overflow-hidden ">
+  <div className="absolute inset-0 z-0">
+    {[...Array(100)].map((_, i) => (
+      <div
+        key={i}
+        className="absolute w-[2px] h-[2px] bg-white rounded-full animate-pulse"
+        style={{
+          top: `${Math.random() * 100}%`,
+          left: `${Math.random() * 100}%`,
+          animationDuration: `${1 + Math.random() * 2}s`,
+        }}
+      />
+    ))}
+  </div>
+
+  {/*pages*/}
+  <Navbar/>
+  <Hero/>
+   <Works/>
+   <Feature/>
+   <SocialTools/>
+   <Testimonial/>
+   <Tail/>
+   
+   
+  
+</div>
+
+  );
 }
+
 export default App;
